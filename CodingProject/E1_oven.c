2a122356008f73138954b9cbbc83ec9a9d8df390
nextop1029384756 <72678118+nextop1029384756@users.noreply.github.com>
2020-10-11 11:40:37 +0900

    Add files via upload

---
+++ CodingProject/E1_oven.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int hour, min, pmin;
+
+	scanf("%d%d%d", &hour, &min, &pmin);
+
+	hour = hour + (min + pmin) / 60;
+	min = (min + pmin) % 60;
+
+	printf("%d %d", hour, min);
+
+	return 0;
+}
