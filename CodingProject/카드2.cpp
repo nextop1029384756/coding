a5888328a105e8cea04359687ea4a875fb5c66ff
nextop1029384756 <72678118+nextop1029384756@users.noreply.github.com>
2021-01-09 13:21:50 +0900

    Add files via upload

--- CodingProject/카드2.cpp
+++ CodingProject/카드2.cpp
@@ -1,47 +1,52 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <string>
-#include <stack>
+#include <vector>
 
 using namespace std;
 
 int main(void)
 {
 	int n, i;
-	int A[100] = {0};
 
 	cin >> n;
 
+	vector<int> A(n);
+
 	for (i = 0; i < n; i++)
 	{
-		A[i] = i;
+		A[i] = i+1;
 	}
+
+	// 123456
+	// 34562
+	// 5624
+	// 246
+	// 4
 
 	while (1)
 	{
-		for (i = 1; i <= n; i++)
+		for (i = 0; i < n; i++) // 4
 		{
-			if(i%2==1)
+			if (i % 2 == 0)
 			{
-				A[n] = 0;
-				A[i + 1] = A[i];
-				n--;
+				// 234
+				for (size_t j = i; j < A.size()-1; j++) {
+					A[j] = A[j + 1];
+				}
+				A.pop_back();
 			}
+			
 
-			if(i%2==0)
-			{
-				A[i + 1] = A[i];			n=4 1234 123 n=3 0123 3123 312 31 n=2 031 231 23 
-				A[1] = A[n];
-				A[n] = 0;
-				n--;
-			}	
-
-			if (n == 1)
-				break;
 		}
+		if (A.size() <= 1) {
+			cout << A[0];
+			return 0;
+		}
+
+		n /= 2;
 	}
 	
-	cout << 'n';
+	
 
 
 	return 0;
